b42ab371284348423c2efa6d330d6d1330089529
Tony Nelson <hhubris@gmail.com>
2011-02-14 11:24:14 -0500

    Script can now detect replica sets with no masters (if that's possible) Added the license to the script

--- README.markdown
+++ README.markdown
@@ -36,7 +36,7 @@ Reload your Nagios configuration and the new monitor should be running.
 
 # License
 
- Copyright 2011 Tony Nelson.
+ Copyright (C) 2011 Tony Nelson.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- check_mongo_replica_stat.rb
+++ check_mongo_replica_stat.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/env ruby
 
+#   Copyright (C) 2011 Tony Nelson
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 require 'net/http'
 require 'optparse'
 
@@ -27,18 +42,28 @@ end
 data.gsub!(':', '=>')
 data.gsub!('Date', '')
 
-
-status = eval(data)
+begin
+  status = eval(data)
+rescue Exception => e
+  puts "ERROR: Unable to eval result from server"
+  exit 2
+end
 
 rc = 0
+have_master = false
 status['members'].each do |member|
   if member['state'] > 2
     print "ERROR: #{member['name']} state #{member['state']} "
     rc = 2
   end
+
+  have_master |= (member['state'] == 1)
 end
 
-if (rc == 0)
+if (!have_master)
+  puts "ERROR: No master detected"
+  rc = 2
+elsif (rc == 0)
   puts 'ReplicaSet OK'
 else
   puts ''
